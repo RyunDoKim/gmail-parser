c0b046af2b308e616d9c0555621c76fb3b0bc8ed
RyunDoKim <ryundokim@gmail.com>
2017-08-11 11:25:18 +0900

    1st commit version 1.0.4

---
+++ README.md
@@ -0,0 +1,200 @@
+# gmail-parser
+
+Parse raw Gmail into a Mail object. Raw mail is retrieved by using Gmail API 'messages.get()' with query param "format" : 'raw' 
+* Tested with 5000 mails. 
+* This is for a 'RAW' Gmail which is retrievable by using Gmail API ['gmail.users.messages.get'](https://developers.google.com/gmail/api/v1/reference/users/messages/get?hl=ko)
+with 'format :raw' query param.
+* Attached File will be returned as raw. Check 'Content Type(content-type of Mail object)' to use it. 
+* Use Iconv to support non UTF-8 charset.  
+***NOTE***  If your mail have charset which Iconv does not support, It will emmit an error.  
+***NOTE*** Charset 'KS_C_5601-1987' will be decoded as 'CP949'. Charset 'KSC5601' and 'KSC5636' will be decoded as EUC-KR. (yes, I am Korean)
+
+## Installation
+
+`npm install gmail-parser`
+
+## Usage
+
+#### parseGmail(rawMail,callback)
+Parse Gmail.raw into a Mail object. 
+
+***rawMail*** : Response from Gmail API  ['gmail.users.messages.get'](https://developers.google.com/gmail/api/v1/reference/users/messages/get?hl=ko) with 'format :raw' query param.  
+***callback*** : Will get error and mail. Even on error, it will take mail imperfectly.
+
+```javascript
+const gmailParser = require('gmail-parser');
+
+gmailParser.parseGmail(rawMail,(err, mail)=>{
+    if(err){
+        console.error(err); 
+        console.log(mail) //Imperfect, but at least it contains the messageId.
+    }
+    console.dir(mail);
+});
+
+let mail = gmailParser.parseGmail(rawMail);
+```
+
+* Example
+```javascript
+
+const gmailParser = require('gmail-parser');
+gmail.users.messages.get({auth:OAuth2, userId:'me', id:messageId, format:'raw'},(err, rawMail)=>{
+    gmailParser.parseGmail(rawMail,callback);
+    //or let mail = gmailParser.parseGmail(rawMail);
+    //doSomething(mail);
+})
+```
+#### parseStringGmail(stringMail, messageId ,callback)
+Takes String type raw Gmail. 
+The value of property 'raw' of response from Gmail API users.messages.get() Method.
+
+***stringMail*** : Value of property 'raw' of response from Gmail API  ['gmail.users.messages.get'](https://developers.google.com/gmail/api/v1/reference/users/messages/get?hl=ko) with 'format :raw' query param.  
+***messageId*** : Gmail messageId. You can use your own custom Id for the Mail object. 
+***callback*** : Will get error and mail. Even on error, it will take mail imperfectly.
+
+```javascript
+const gmailParser = require('gmail-parser');
+
+gmailParser.parseStringGmail(stringMail, messageId ,(err, mail)=>{
+    if(err){
+        console.error(err);
+        console.log(mail) //Imperfect, but at least it contains the messageId.
+    }
+    console.dir(mail);
+});
+
+let mail = gmailParser.parseGmail(StringMail, messageId);
+```
+
+* Example
+```javascript
+
+const gmailParser = require('gmail-parser');
+gmail.users.messages.get({auth:OAuth2, userId:'me', id:messageId, format:'raw'},(err, rawMail)=>{
+    gmailParser.parseStringGmail(rawMail.raw,rawMail.id,callback);
+    //or let mail = gmailParser.parseGmail(rawMail.raw, messageId);
+    //or let mail = gmailParser.parseGmail(rawMail.raw, messageId);
+    //doSomething(mail);
+})
+```
+* Case when you want to impose your own custom id. 
+
+```javascript
+
+const gmailParser = require('gmail-parser');
+gmail.users.messages.get({auth:OAuth2, userId:'me', id:messageId, format:'raw'},(err, rawMail)=>{
+    gmailParser.parseStringGmail(rawMail.raw, customId, callback);
+    //or let mail = gmailParser.parseGmail(rawMail.raw, customId);
+    //doSomething(mail);
+})
+```
+## Mail
+
+* Mail is an object holding parsed information.
+* It has the header information as it's property.  
+***Note*** The name(identifier) of properties are all lower case.
+* Most of properties it has are string type.
+* 'Content-Type' should be a object which has 'type' property (ex. text/plain) and each attributes this header has.
+* Depends of the 'Content-type' ('content-type' in object), the content can be either string or array.
+
+* Content as string
+```javascript
+console.log(mail.content);
+```
+```javascript
+{
+  "messageid": "15da1a51245c12a2d",
+  "date": "Wed, 9 Aug 2017 00:00:01 +0900",
+  "from": "sample from <from@sample.com>",
+  "subject": "sample subject",
+  "to": "to@sample.com",
+  "content-type": {
+    "type": "text/plain",
+    "charset": "utf-8"
+  },
+  "content-transfer-encoding": "base64",
+  "content": "sample content",
+}
+```
+
+* Content as Array  
+***Content can be an array when the content-type is 'multipart' each part is parsed into a Mail object. Sometimes you will see dual multiparts***
+```javascript
+mail.forEach((content)=>{
+        console.log(content);
+    });
+```
+
+```javascript
+{   "messageid": "15dc1377342ea701",
+    "date": "Wed, 9 Aug 2017 00:00:01 +0900",
+    "from": "sample from <from@sample.com>",
+    "subject": "sample subject",
+    "to": "to@sample.com",
+    "reply-to": "sample.com <replyto@sample.com>",
+    "cc": "cc@sample.com <cc@sample.com>",
+    "mime-version": "1.0",
+    "content-type": {
+      "type": "multipart/alternative",
+      "boundary": "b2f86160fec5cb3eefd91aa29693ad5b8d25b6a836e0f64748b5aeab6c7e"
+    },
+    "content": [
+      {
+        "messageid": "15dc1377342ea701",
+        "date": "Wed, 9 Aug 2017 00:00:01 +0900",
+        "from": "sample from <from@sample.com>",
+        "subject": "sample subject",
+        "to": "to@sample.com",
+        "reply-to": "sample.com <replyto@sample.com>",
+        "cc": "cc@sample.com <cc@sample.com>",
+        "mime-version": "1.0",
+        "content-type": {
+          "type": "text/plain",
+          "charset": "UTF-8"
+        },
+        "content-transfer-encoding": "quoted-printable"
+        "content": "inner content 1"
+      },
+      {
+        "messageid": "15dc1377342ea701",
+        "date": "Wed, 9 Aug 2017 00:00:01 +0900",
+        "from": "sample from <from@sample.com>",
+        "subject": "sample subject",
+        "to": "to@sample.com",
+        "reply-to": "sample.com <replyto@sample.com>",
+        "cc": "cc@sample.com <cc@sample.com>",
+        "mime-version": "1.0",
+        "content-type": {
+          "type": "multipart/alternative",
+          "boundar": "123112cbab4ca812ba1231412bcaccab12314231eea"
+        },
+        "content": [...]
+      }
+    ]
+    }
+```
+
+## License
+ MIT License
+ Copyright (c) <2017> <RyunDo Kim>
+ Permission is hereby granted, free of charge, to any person
+ obtaining a copy of this software and associated documentation
+ files (the "Software"), to deal in the Software without
+ restriction, including without limitation the rights to use,
+ copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following
+ conditions:
+
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,302 @@
+/**
+ * Created by RyunDo Kim<ryundokim@gmail.com> on 2017. 8. 4.
+ * @author RyunDo Kim(dodo)
+ * @licence MIT License
+ Copyright (c) <2017> <RyunDo Kim>
+ Permission is hereby granted, free of charge, to any person
+ obtaining a copy of this software and associated documentation
+ files (the "Software"), to deal in the Software without
+ restriction, including without limitation the rights to use,
+ copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following
+ conditions:
+
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+const quotedPrintable = require("quoted-printable");
+const Iconv = require("iconv").Iconv;
+const dateRegEx = /(Date):\s([^\n\r]+?)\r?\n/i;
+const fromRegEx = /(From):\s([^\n\r]+?)\r?\n/i;
+const toRegEx = /(To):\s([^\n\r]+?)\r?\n/i;
+const replyToRegEx = /(Reply-To):\s([^\n\r]+?)\r?\n/i;
+const ccRegEx = /(CC):\s([^\n\r]+?)\r?\n/i;
+const mimeVersionRegEx = /(Mime-Version):\s([^\n\r]+?)\r?\n/i;
+const subjectRegEx = /(Subject):\s((=\?[\s\S]+\?=)|([^\n\r]+))/i;
+const titleEncodingRegEx = /=\?(.*?)\?([BQ])\?([\S]+?)\?=/gi;
+const contentTypesRegEx = /(Content-Type):\s([^;\s]*?([;\s]+[^:\s]+)?)\s+/i;
+const contentTypeAttrRegEx = /([^=;\s]+)="?([^;"\s]+)"?;?/gi;
+const contentTypeRegEx = /([^;\s]+);?/i;
+const contentTransferEncodingRegEx = /(Content-Transfer-Encoding):\s([^;\s]*)\s/i;
+const contentRegEx = /(?:\r?\n){2,}([\s\S]+)/i;
+
+
+/**
+ * @class Mail
+ * @typedef {object} Mail
+ * @version 0.0.1
+ * @description A class contains properties of parsed mail. All property names are lowercase. (ex. content-type, messageid)
+ * @description 문자열 메일을 파싱한 결과가 담길 객체 클래스입니다. 속성 식별자는 모두 소문자입니다. (ex. content-type, messageid)
+ * @property {string} messageid messageId Gmail messageId, not a messageId in raw  mail, it is retrievable through gmail.users.messages.list) method.
+ * gmail의 메세지 아이디입니다.  원형 데이터의 것이 아닌 gmail.users.messages.list로 구할 수 있는 아이디입니다.
+ * @property {string|Array} content If the Content-Type of mail is multipart, this will be an Array of sub mails(Mail object which hold each parsed content information), if not, this will be a string
+ * multipart일 경우 여러 메일의 배열을 갖게 되며 그렇지 않을경우 보통의 경우 문자열입니다
+ **/
+class Mail {
+    constructor(messageId) {
+        this.messageid = messageId;
+    }
+
+    /**
+     * @description  Parse string content-type information and make a object to hold that. If it has sub attributes (ex. charset, boundary), these will be each property of the object
+     * parseContentType 으로 타입 정보 외에 여러 요소를 갖고 있는 문자열의 content-type을 파싱해 객체로 만들어줍니다. 각각의 요소들은 객체의 속성이됩니다
+     **/
+    parseContentType() {
+        let contentType;
+        let attr;
+        contentType = {type: contentTypeRegEx.exec(this["content-type"])[1]};
+        while ((attr = contentTypeAttrRegEx.exec(this["content-type"])) !== null) {
+            contentType[attr[1]] = decodeTitle(attr[2]);
+        }
+        this["content-type"] = contentType;
+    }
+}
+
+
+/**
+ * @description Parse raw  mail. Returns Mail object which contains parsed information.
+ * 문자열의 메일을 (rawMail)을 분석해 전달받은 Mail 객체에 해당 분석 정보를 담습니다.
+ * @param {string} rawMail Decoded raw  mail data. (decoded by base64url).
+ * 디코딩된 문자열 메일입니다. base64url로 디코드 되었습니다
+ * @param {string} messageId Gmail messageId, not a messageId in raw  mail, it is retrievable through gmail.users.messages.list) method.
+ * mail 문자열 메일을 분석해 정보를 담을 객체입니다.
+ * @param {function} callback Callback when got an error. Params are error and Mail object.
+ * 에러 발생시 호출될 콜백. 에러와 함께 만들고 있던 Mail 객체를 전달합니다.
+ * @returns {Mail} Return Mail object containing parsed information.
+ * 분석 결과를 담고 있는 Mail 객체를 반환합니다.
+ **/
+function parseToMailObj(rawMail, messageId, callback) {
+    "use strict";
+    let mail = new Mail(messageId);
+    try {
+        execRegEx(rawMail, dateRegEx, mail);
+        execRegEx(rawMail, toRegEx, mail);
+        execRegEx(rawMail, replyToRegEx, mail);
+        execRegEx(rawMail, ccRegEx, mail);
+        execRegEx(rawMail, mimeVersionRegEx, mail);
+        execRegEx(rawMail, fromRegEx, mail);
+        mail.from = decodeTitle(mail.from);
+        execRegEx(rawMail, subjectRegEx, mail);
+        mail.subject = decodeTitle(mail.subject);
+        parseContent(rawMail, mail);
+    } catch (e) {
+        callback(e,mail);
+    }
+    return mail;
+}
+/**
+ * @description Decode subject of mail or title of attached file.
+ * 메일의 제목 또는 첨부파일의 이름을 디코딩합니다.
+ * @param title {string} subject of mail or title of attached file.
+ * 메일의 제목 또는 첨부파일의 이름
+ * @return {string} If decodable, it return decoded or the original title.
+ * 디코딩할 수 있다면 디코딩된 값을 그렇지 않다면 기존 문자열을 반환합니다
+ **/
+function decodeTitle(title) {
+    "use strict";
+    let temp = "";
+    let result = titleEncodingRegEx.exec(title);
+    while (result !== null) {
+        temp += decodeByEncoding(result[3], result[2], result[1].toUpperCase());
+        result = titleEncodingRegEx.exec(title)
+    }
+    return (temp.length > 0) ? temp : title;
+}
+
+/**
+ * @description Parse content. Parsed content will be contained the assigned Mail object (mail)
+ * 문자열의 메일 본문을 (content) 분석해 전달받은 객체(mail)의 content에 분석 정보를 담습니다
+ * @param {string} content, will be parsed.
+ * content 분석할 문자열의 메일 본문(또는 첨부파일)입니다.
+ * @param {Mail} mail A Mail object. This will contain parsed content information.
+ * mail 분석 결과를 담을 객체입니다.
+ * @return {Mail} Return the Mail object.
+ * 분석 결과를 담은 Mail 객체를 반환합니다.
+ **/
+function parseContent(content, mail) {
+    "use strict";
+    execRegEx(content, contentTypesRegEx, mail);
+    mail.parseContentType();
+    let contentType = mail["content-type"];
+    if (contentType.type.match("multipart/")) {
+        mail.content = splitMultipartContent(content, mail);
+    } else if (contentType.type.match("text/")) {
+        if (execRegEx(content, contentTransferEncodingRegEx, mail)) {
+            content = contentRegEx.exec(content)[1];
+            content = decodeByEncoding(content, mail["content-transfer-encoding"], mail["content-type"].charset);
+        }
+        mail.content = content;
+    } else {
+        content = contentRegEx.exec(content)[1];
+        mail.content = content;
+    }
+    return mail;
+}
+/**
+ * @description Parse string using Regular Expressions and compose mail object using the result.
+ * RegExp를 이용해 문자열을 분석하고 결과에 맞춰 Mail 객체를 구성합니다
+ * @param {string} rawMail Raw mail
+ * 분석되지 않은 메일
+ * @param {RegExp} regEx Regular Expressions to parse raw mail.
+ * 분석을 위한 정규표현식
+ * @param {Mail} mail Mail object to hold parsed information.
+ * 분석된 결과를 담을 객체
+ * @returns {Array} Return the result of Regular Expression execution(RegExp.exec()). Null means the parsing fails.
+ * 정규표현식 결과를 반환합니다. Null은 분석의 실패를 의미합니다
+ **/
+function execRegEx(rawMail, regEx, mail) {
+    "use strict";
+    let result = regEx.exec(rawMail);
+    if (!result) {
+        return null;
+    }
+    composeMail(result, mail);
+    return result;
+}
+
+/**
+ * @description Compose Mail object using Regular Expression result. All property names are lowercase.
+ * RegEx 결과값을 이용해 Mail 객체를 구성합니다. 속성 식별자는 모두 소문자입니다
+ * @param {Array} regExResult Regular Expression result. (RegExp.exex())
+ * 정규표현식 결과입니다
+ * @param {Mail} mail Mail object to hold parsed information.
+ * 분석된 정보를 가지고 있는 Mail 객체입니다
+ **/
+function composeMail(regExResult, mail) {
+    "use strict";
+    let tag = "";
+    for (let idx = 0; idx < regExResult.length; idx++) {
+        if (regExResult[idx] === undefined) {
+            continue;
+        }
+        if (idx % 3 === 1) {
+            tag = regExResult[idx].toLowerCase();
+        } else if (idx % 3 === 2) {
+            mail[tag] = regExResult[idx];
+        }
+    }
+}
+
+/**
+ * @description Split multipart type content by boundary and parse content. Each split and parsed content will be a Mail object and stored in an Array.
+ * multipart 타입의 본문을 분할합니다. mail에 저장된 boundary를 사용합니다. 그후 본문을 분할된 본문을 각각 파싱해 객체에 저장합니다. 저장된 분할된 본문은 각각 하나의 Mail객체로 배열에 저장됩니다
+ * @param {string} rawMail raw mail
+ * 분석될 메일입니다.
+ * @param {Mail} mail Mail object to hold parsed information. the property "content-type" will be an Array. In some cases it can be multi-dimensional (especially has attached file).
+ * 분석된 결과를 담을 객체입니다 "content-type"은 배열이됩니다. 경우에 따라 다차원이 될 수 있습니다. (첨부파일이 있는 경우 특히 그러합니다).
+ * @returns {Array} Return the array containing split contents(Mail object).
+ * 분할되어 각긱 하나의 메일로 분석된 객체들을 갖고 있는 배열을 반환합니다
+ **/
+function splitMultipartContent(rawMail, mail) {
+    "use strict";
+    let boundary = "--" + mail["content-type"].boundary;
+    let subs = rawMail.split(boundary);
+    let contents = [];
+    subs.shift();
+    subs.pop();
+    subs.forEach((sub) => {
+        let subMail = new Mail(mail.messageid);
+        for (let property in mail) {
+            if (!mail.hasOwnProperty(property)) {
+                continue;
+            }
+            subMail[property] = mail[property];
+        }
+        contents.push(parseContent(sub, subMail));
+    });
+    return contents;
+}
+
+/**
+ * @description Decode raw data as encoded way.
+ * 인코딩된 방식에 따라 디코딩합니다.
+ * @param {string} raw Encoded data.
+ * 인코딩된 데이터입니다
+ * @param {string} encodingOption Encoding option.
+ * 인코딩 방식입니다
+ * @param {string} charset of the encoding. If null, it will be UTF-8;
+ * 문자집합 이름입니다. null의 경우 UTF-8로 처리됩니다
+ * @returns {string} Return decoded data.
+ * 디코딩된 데이터를 반환합니다.
+ **/
+function decodeByEncoding(raw, encodingOption, charset) {
+    let buffer = null;
+    switch (encodingOption) {
+        case "B":
+        case "base64":
+            buffer = new Buffer(raw, "base64");
+            break;
+        case "Q":
+        case "quoted-printable":
+            buffer = new Buffer(quotedPrintable.decode(raw), "binary");
+            break;
+        default: // 7bit, 8bit, binary
+            buffer = new Buffer(raw, "binary");
+    }
+    if (charset && !charset.match(/UTF-?8/i)) {
+        if(charset.match(/KS_C_5601-1987/i)){
+            charset = "CP949";
+        }else if(charset.match(/(KSC5601|KSC5636)/i)){
+            charset = "EUC-KR";
+        }
+        let iconv = new Iconv(charset, "UTF-8//TRANSLIT//IGNORE");
+        buffer = iconv.convert(buffer);
+    }
+    return buffer.toString();
+}
+
+/**
+ * @description Parse Gmail.raw into Mail object.
+ * Gmail의 raw 데이터를 분석해 Mail 객체로 만듭니다
+ * @param {object} rawMail the result of Gmail Api users.messages.get() with query param format: "raw".
+ * 쿼리 인수로 format: "raw"를 사용한 Gmail Api users.messages.get() 응답 결과입니다
+ * @param {function} callback
+ * @returns {Mail} Return Parsed Mail object.
+ */
+exports.parseGmail = function (rawMail, callback) {
+    let decodedRawMail = decodeByEncoding(rawMail.raw, "B");
+    let mail = parseToMailObj(decodedRawMail, rawMail.id, callback);
+    if (callback) {
+        callback(null, mail);
+    }
+    return mail;
+};
+/**
+ * @description Parse string raw Gamil into Mail object.
+ * 문자열의 Gmail을 분석해 Mail객체로 만듭니다
+ * @param {string} stringMail String raw Gmail. Value of Gmail.raw
+ * Gmail.raw의 값인 문자열로된 메일정보입니다
+ * @param {string} messageId Gmail messageId which used as a query param on users.messages.get() method.
+ * Gmail Id입니다.
+ * @param {function} callback
+ * @returns {Mail} Return Parsed Mail object.
+ */
+exports.parseStringGmail = function (stringMail, messageId, callback) {
+    let decodedRawMail = decodeByEncoding(stringMail, "B");
+    let mail = parseToMailObj(decodedRawMail, messageId, callback);
+    if (callback) {
+        callback(null, mail);
+    }
+    return mail;
+};
